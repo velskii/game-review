0f5a46137bc85a84264cd2e021ea8a37828db44d
velskii <feiliangphp@gmail.com>
2023-03-09 10:48:19 -0500

    added the content of week 8

--- server.js
+++ server.js
@@ -5,26 +5,26 @@
  *
  */
 //#endregion
-
 //#region Server Setup
 var express = require("express");
 var app = express();
 var HTTP_PORT = process.env.PORT || 8080;
+
 require("dotenv").config();
+
 app.use(express.static("views"));
 app.use(express.static("public"));
 
 const { engine } = require("express-handlebars");
-
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
-const bodyParser = require("body-parser");
+var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const clientSessions = require("client-sessions");
+const clientSession = require("client-sessions");
 app.use(
-  clientSessions({
+  clientSession({
     cookieName: "Cap805Session",
     secret: "cap805_week8_mongodbDemo",
     duration: 2 * 60 * 1000,
@@ -32,31 +32,33 @@ app.use(
   })
 );
 
-//#region Mongoose
+// Mongoose
 const mongoose = require("mongoose");
 mongoose.connect(process.env.dbConn, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const UserModel = require("./models/userModel");
-//endregion
 
 //#region TEMPORARY USER MODEL
+/*
 const usr = {
-  username: "greg",
-  password: "123456",
-  firstName: "Feiliang",
-  lastName: "Zhou",
-  email: "fzhou30@myseneca.ca",
-  isAdmin: false,
+    username: "clint",
+    password: "mypassword",
+    email: "clint.macdonald@senecacollege.ca",
+    isAdmin: false,
+    firstName: "Clint",
+    lastName: "MacDonald"
 };
+*/
 //#endregion
 
 //#endregion
 
 //#region Custom Server Functions
 function OnHttpStart() {
-  console.log("Express started successfully on port: " + HTTP_PORT);
+  console.log("Express server started successfully on port: " + HTTP_PORT);
 }
 
 function ensureLogin(req, res, next) {
@@ -69,99 +71,198 @@ function ensureLogin(req, res, next) {
 
 function ensureAdmin(req, res, next) {
   if (!req.Cap805Session.user.isAdmin) {
-    res.redirect("/login");
+    res.render("login", {
+      errorMsg: "You do not have permissions for the page requested!",
+      layout: false,
+    });
   } else {
     next();
   }
 }
-//endregion
+
+//#endregion
 
 //#region ROUTES
 app.get("/", (req, res) => {
-  res.render("home", { layout: false, user: req.Cap805Session.user });
+  res.render("home", { user: req.Cap805Session.user, layout: false });
 });
 app.get("/about", (req, res) => {
-  res.render("about", { layout: false, user: req.Cap805Session.user });
+  res.render("about", { user: req.Cap805Session.user, layout: false });
 });
 app.get("/contact", (req, res) => {
-  res.render("contact", { layout: false, user: req.Cap805Session.user });
+  res.render("contact", { user: req.Cap805Session.user, layout: false });
 });
 
 //#region USER ROUTES
 app.get("/login", (req, res) => {
-  res.render("login", { layout: false, user: req.Cap805Session.user });
+  res.render("login", { user: req.Cap805Session.user, layout: false });
+});
+app.get("/logout", (req, res) => {
+  req.Cap805Session.reset();
+  res.redirect("/");
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
+
   if (username === "" || password === "") {
-    res.render("login", { layout: false });
+    res.render("login", {
+      errorMsg: "Both the login and password are required fields",
+      layout: false,
+    });
   }
-  if (username == usr.username && password == usr.password) {
-    req.Cap805Session.user = {
-      username: usr.username,
-      email: usr.email,
-      firstName: usr.firstName,
-      lastName: usr.lastName,
-      isAdmin: usr.isAdmin,
-    };
-    if (usr.isAdmin) {
-      res.redirect("/admin/dashboard");
-    } else {
-      res.redirect("/dashboard");
-    }
-    console.log("login successfully");
-  } else {
-    res.render("login", { layout: false });
-  }
+
+  // go to database
+  UserModel.findOne({ username: username })
+    .exec()
+    .then((usr) => {
+      // check username
+      if (usr) {
+        if (password === usr.password) {
+          req.Cap805Session.user = {
+            username: usr.username,
+            email: usr.email,
+            isAdmin: usr.isAdmin,
+            firstName: usr.firstName,
+            lastName: usr.lastName,
+          };
+
+          if (usr.isAdmin) {
+            res.redirect("/admin/dashboard");
+          } else {
+            res.redirect("/dashboard");
+          }
+          console.log("Login was successful");
+        } else {
+          res.render("login", {
+            errorMsg: "User credentials are incorrect! Password was incorrect",
+            layout: false,
+          });
+        }
+      } else {
+        res.render("login", {
+          errorMsg: "User credentials are incorrect! Login was not found",
+          layout: false,
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("An error occurred: ${err}" + err);
+    });
 });
 
-app.get("/logout", (req, res) => {
-  req.Cap805Session.reset();
-  res.redirect("/");
-});
 //#endregion
 
-//#region USER AFTER LOGIN PAGES
-app.get("/profile", (req, res) => {
-  res.render("profile", { layout: false, user: req.Cap805Session.user });
-});
-
+//#region USER Pages
 app.get("/dashboard", ensureLogin, (req, res) => {
-  res.render("dashboard", { layout: false, user: req.Cap805Session.user });
+  res.render("dashboard", { user: req.Cap805Session.user, layout: false });
 });
-app.get("/admin/dashboard", ensureLogin, (req, res) => {
-  res.render("admin/dashboard", {
-    layout: false,
-    user: req.Cap805Session.user,
-  });
+
+app.get("/admin/dashboard", ensureAdmin, (req, res) => {
+  UserModel.find()
+    .lean()
+    .exec()
+    .then((users) => {
+      res.render("adminDashboard", {
+        user: req.Cap805Session.user,
+        hasUsers: !!users.length,
+        users: users,
+        layout: false,
+      });
+    });
+});
+
+app.get("/profile", ensureLogin, (req, res) => {
+  res.render("profile", { user: req.Cap805Session.user, layout: false });
+});
+
+app.get("/profile/edit", ensureLogin, (req, res) => {
+  res.render("profileedit", { user: req.Cap805Session.user, layout: false });
+});
+
+app.post("/profile/edit", ensureLogin, (req, res) => {
+  const username = req.body.username;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const Email = req.body.email;
+  const isAdmin = req.body.isAdmin == "on";
+
+  if (isAdmin) {
+    UserModel.updateOne(
+      { username: username },
+      {
+        $set: {
+          firstName: firstName,
+          lastName: lastName,
+          email: Email,
+          isAdmin: true,
+        },
+      }
+    )
+      .exec()
+      .then(() => {
+        req.Cap805Session.user = {
+          username: username,
+          email: Email,
+          firstName: firstName,
+          lastName: lastName,
+          isAdmin: true,
+        };
+        res.redirect("/Profile");
+      });
+  } else {
+    UserModel.updateOne(
+      { username: username },
+      {
+        $set: {
+          firstName: firstName,
+          lastName: lastName,
+          email: Email,
+          isAdmin: false,
+        },
+      }
+    )
+      .exec()
+      .then(() => {
+        req.Cap805Session.user = {
+          username: username,
+          email: Email,
+          firstName: firstName,
+          lastName: lastName,
+          isAdmin: false,
+        };
+        res.redirect("/Profile");
+      });
+  }
 });
 //#endregion
-
 //#endregion
 
 //#region TEMP ONE TIME RUN ONLY
+/*
 app.get("/firstrunsetup", (req, res) => {
-  var Greg = new UserModel({
-    username: usr.username,
-    password: usr.password,
-    email: usr.email,
-    firstName: usr.firstName,
-    lastName: usr.lastName,
-    isAdmin: usr.isAdmin,
-  });
-  Greg.save((err) => {
-    console.log("Error: " + err + ";");
-    if (err) {
-      console.log("There was an error creating Greg: " + err);
-    } else {
-      console.log("Greg was created successfully");
-    }
-    console.log("Got here after creating Greg");
-    res.redirect("/");
-  });
+    var Clint = new UserModel({
+        username: 'clint',
+        password: 'mypassword',
+        firstName: 'Clint',
+        lastName: 'MacDonald',
+        email: 'clint.macdonald@senecacollege.ca',
+        isAdmin: false
+    });
+
+    Clint.save((err) => {
+        console.log("Error: " + err + ";");
+        if (err) {
+            console.log("There was an error creating Clint: " + err);
+        } else {
+            console.log("Clint was created successfully");
+        }
+        console.log("Get here after saving Clint");
+        res.redirect("/");
+    } )
 });
+*/
 //#endregion
 
 app.listen(HTTP_PORT, OnHttpStart);
